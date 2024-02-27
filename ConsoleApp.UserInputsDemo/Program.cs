8ebadcb18720d85642e08d6ad969c17a987e8a85
rohasent <rohasent@BGHLPG02VYG9>
2024-02-27 15:54:57 +0530

    Add project files.

---
+++ ConsoleApp.UserInputsDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = string.Empty;
+int age  = 0;
+int retirement = 60;
+
+//prompt for user inputs
+
+Console.WriteLine("Enter the name");
+name = Console.ReadLine();
+
+Console.WriteLine("Enter the age");
+age = Convert.ToInt32(Console.ReadLine());
+
+//process the inputs
+
+int remaining = retirement - age; 
+Console.WriteLine("User name :" + name);
+Console.WriteLine("User age : " + age);
+Console.WriteLine("remaining years :" + remaining);  
---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//datatypes
+// int,string,decimel,float,logical,double
+
+string name = "Rohan";
+Console.WriteLine(name);
+Console.WriteLine("He is " + name); //string concatenation
+Console.WriteLine($"His neighbour is also {name}");//string interpolation
+Console.WriteLine("Both {0}'s nickname is Rocky", name);//formatted string
+
+int nu = 10;
+Console.WriteLine("the number is :" + nu);
+
+bool isNumber = true;
+Console.WriteLine("The number " + nu + " is greater than 9?" + isNumber);
