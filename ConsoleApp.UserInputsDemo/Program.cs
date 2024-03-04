03537f8c83a5e1dc0f89ee0a97d820ab6d380799
Rohan Kumar Senthilkumar <rohan-kumar.senthilkumar@capgemini.com>
2024-03-04 12:10:01 +0530

    Added operators

--- ConsoleApp.UserInputsDemo/Program.cs
+++ ConsoleApp.UserInputsDemo/Program.cs
@@ -1,19 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string name = string.Empty;
-int age  = 0;
+string firstname = string.Empty;
+string lastname = string.Empty;
+int age = 0;
 int retirement = 60;
+decimal salary = 0;
+char gender =  char.MinValue;
+bool working = true;
 
 //prompt for user inputs
 
-Console.WriteLine("Enter the name");
-name = Console.ReadLine();
+Console.Write("Enter the first name: ");
+firstname = Console.ReadLine();
 
-Console.WriteLine("Enter the age");
+Console.Write("Enter the last name: ");
+lastname = Console.ReadLine();
+
+Console.Write("Enter the age: ");
 age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the salary: ");
+salary = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you working (True or False): ");
+working = Convert.ToBoolean(Console.ReadLine());
 
 //process the inputs
 
 int remaining = retirement - age; 
-Console.WriteLine("User name :" + name);
+Console.WriteLine($"User name : { firstname} { lastname}");
 Console.WriteLine("User age : " + age);
-Console.WriteLine("remaining years :" + remaining);  
+Console.WriteLine("remaining years :" + remaining);
+Console.WriteLine("User salary : " + salary);
+Console.WriteLine("User gender : " + gender);
+Console.WriteLine("User working status : " + working);
---
+++ ConsoleAppOperators/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.Write("Enter the first number: ");
+Decimal num1=Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+Decimal num2 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine();
+
+//arithmetic
+
+Console.WriteLine("***********Math Operators**********");
+Console.WriteLine();
+Decimal sum = num1+num2;
+Console.WriteLine("The sum is "+sum);
+
+Decimal product = num1 * num2;
+Console.WriteLine("The product is "+product);
+
+Decimal div=num1 / num2;
+Console.WriteLine("The Division is " + div);
+
+Decimal sub=num1 - num2;
+Console.WriteLine("The subtraction is "+sub);
+
+Decimal mod=num1 % num2;
+Console.WriteLine("The Modulas is "+mod);
+Console.WriteLine();
+//logic operators
+
+var isGreaterthan = num1 > num2;
+var  isLessthan = num1 < num2;
+var isEqualto  = num1 == num2;
+var isGreaterOrEqualTo =    num1 >= num2;
+var isLesserOrEqualTo = num1 <= num2;
+var isNotEqual =  num1 != num2;
+
+Console.WriteLine();
+Console.WriteLine("***********Logic Operators**********");
+Console.WriteLine();
+Console.WriteLine($"Is greaterthan value : {isGreaterthan}" );
+Console.WriteLine($"Is Lesserthan value : {isLessthan}");
+Console.WriteLine($"Is Equal to value : {isEqualto}");
+Console.WriteLine($"Is greaterthan or Equal to value : {isGreaterOrEqualTo}");
+Console.WriteLine($"Is Lesserthan or equal to value : {isLesserOrEqualTo}");
+Console.WriteLine($"Is Not equal to value : {isNotEqual}");
+Console.WriteLine();
+
+//Assigning operators
+
+Console.WriteLine("Enter the random value for assigning operators");
+int random = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("***********Assigning Operators**********");
+Console.WriteLine();
+num1 += random;
+Console.WriteLine($"The num1 increased by random is {num1}");
+num1 -= random;
+Console.WriteLine($"The num1 decreased by random is {num1}");
+num1 *= random;
+Console.WriteLine($"The num1 Multiplied by random is {num1}");
+num1 /= random;
+Console.WriteLine($"The num1 Divided by random is {num1}");
+num1 %= random;
+Console.WriteLine($"The num1 modulus by random is {num1}");
+Console.WriteLine();
