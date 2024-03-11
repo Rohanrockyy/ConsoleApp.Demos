ee9b5e5e6417ddb8e528c11abdac9b042501dac3
Rohan Kumar Senthilkumar <rohan-kumar.senthilkumar@capgemini.com>
2024-03-11 15:10:08 +0530

    Added loops and modified sample calculator

---
+++ ConsoleAppLoops/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("How many times you want to print hello world");
+int count = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Hello world {i}");
+}
+Console.WriteLine("Exit");
+
+Console.WriteLine("While loop sample");
+int sum = 0;
+int num = 0;
+while(num!= -1)
+{
+    Console.Write("Enter the number to be summed (-1 to exit):");
+    num = Convert.ToInt32(Console.ReadLine());
+    if(num !=-1 )
+    {
+        sum += num;
+    }  
+}
+Console.WriteLine($"Sum of the numbers is : {sum}");
+Console.WriteLine("Exited loop");
+
+Console.WriteLine("Do while sample");
+sum = 0;
+num = 0;
+do
+{
+    Console.Write("Enter the number to be summed (-1 to exit):");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num != -1)
+    {
+        sum += num;
+    }
+}while(num!= -1);
+Console.WriteLine($"Sum is {sum}");
--- ConsoleAppSampleCalculator/Program.cs
+++ ConsoleAppSampleCalculator/Program.cs
@@ -1,41 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the sample calculator");
 
-Console.Write("Select from option");
-Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod");
+Console.Write("Select from option(-1 to exit)");
+Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod \n 6.Fibonacci ");
 Console.Write("Enter the option :");
 int option = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Enter the first number");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("enter the second number");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-int value=0;
-switch(option)
+while (option != -1)
 {
-    case 1:
-        value = num1 + num2;
-        Console.WriteLine($"Addtion of values is {value}");
-        break;
-    case 2:
-        value = num1 - num2;
-        Console.WriteLine($"Subtraction of values is {value}");
-        break;
-    case 3:
-        value = num1 * num2;
-        Console.WriteLine($"Multiplication of values is {value}");
-        break;
-    case 4:
-        value = num1 / num2;
-        Console.WriteLine($"Division of values is {value}");
-        break;
-    case 5:
-        value = num1 % num2;
-        Console.WriteLine($"Modulas of values is {value}");
-        break;
-    default:
-        Console.WriteLine("Invalid option");
-        break;
+    Console.WriteLine("Enter the first number");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("enter the second number");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int value = 0;
+    switch (option)
+    {
+        case 1:
+            value = num1 + num2;
+            Console.WriteLine($"Addtion of values is {value}");
+            break;
+        case 2:
+            value = num1 - num2;
+            Console.WriteLine($"Subtraction of values is {value}");
+            break;
+        case 3:
+            value = num1 * num2;
+            Console.WriteLine($"Multiplication of values is {value}");
+            break;
+        case 4:
+            value = num1 / num2;
+            Console.WriteLine($"Division of values is {value}");
+            break;
+        case 5:
+            value = num1 % num2;
+            Console.WriteLine($"Modulas of values is {value}");
+            break;
+        case 6:
+            for (int i = num1; i <= num2; i++)
+            {
+                value += i;
+            }
+            Console.WriteLine($"Fibonacci value is {value}");
+            break;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+    Console.WriteLine($"Answer for selected option is {value}");
+
+    Console.WriteLine("Press any to continue");
+    Console.ReadLine();
+    Console.Clear();
+    Console.Write("Select from option(-1 to exit)");
+    Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod \n 6.Fibonacci ");
+    Console.Write("Enter the option :");
+    option = Convert.ToInt32(Console.ReadLine());
+
 }
-Console.WriteLine($"Answer for selected option is {value}");
+Console.WriteLine("Thanks for using the calculator");
