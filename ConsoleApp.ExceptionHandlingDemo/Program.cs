f91e1ee367daa282e9fe779908ae2ff1257ac02f
Rohan Kumar Senthilkumar <rohan-kumar.senthilkumar@capgemini.com>
2024-03-11 23:49:04 +0530

    Added exception handling and modified calculator

---
+++ ConsoleApp.ExceptionHandlingDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+    Console.WriteLine("Enter the age: ");
+    int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"The age is {age}");
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Incorrect value. Try again");
+	//throw;(will display the exception while running
+}
+finally
+{
+    Console.WriteLine("Thanks for using");
+}
--- ConsoleAppSampleCalculator/Program.cs
+++ ConsoleAppSampleCalculator/Program.cs
@@ -1,60 +1,89 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to the sample calculator");
+using System.Linq.Expressions;
 
-Console.Write("Select from option(-1 to exit)");
-Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod \n 6.Fibonacci ");
-Console.Write("Enter the option :");
-int option = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Welcome to the sample calculator");
+int option = 0;
+int num1, num2;
 while (option != -1)
 {
-    Console.WriteLine("Enter the first number");
-    int num1 = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("enter the second number");
-    int num2 = Convert.ToInt32(Console.ReadLine());
-
-    int value = 0;
-    switch (option)
+    try
     {
-        case 1:
-            value = num1 + num2;
-            Console.WriteLine($"Addtion of values is {value}");
+        Console.Write("Select from option(-1 to exit)");
+        Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod \n 6.Fibonacci ");
+        Console.Write("Enter the option :");
+        option = Convert.ToInt32(Console.ReadLine());
+        if (option == -1)
+        {
             break;
-        case 2:
-            value = num1 - num2;
-            Console.WriteLine($"Subtraction of values is {value}");
-            break;
-        case 3:
-            value = num1 * num2;
-            Console.WriteLine($"Multiplication of values is {value}");
-            break;
-        case 4:
-            value = num1 / num2;
-            Console.WriteLine($"Division of values is {value}");
-            break;
-        case 5:
-            value = num1 % num2;
-            Console.WriteLine($"Modulas of values is {value}");
-            break;
-        case 6:
-            for (int i = num1; i <= num2; i++)
+        }
+        else if (option >= 1 && option <= 6)
+        {
+            Console.WriteLine("Enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int value = 0;
+            switch (option)
             {
-                value += i;
+                case 1:
+                    value = num1 + num2;
+                    Console.WriteLine($"Addtion of values is {value}");
+                    break;
+                case 2:
+                    value = num1 - num2;
+                    Console.WriteLine($"Subtraction of values is {value}");
+                    break;
+                case 3:
+                    value = num1 * num2;
+                    Console.WriteLine($"Multiplication of values is {value}");
+                    break;
+                case 4:
+                    value = num1 / num2;
+                    Console.WriteLine($"Division of values is {value}");
+                    break;
+                case 5:
+                    value = num1 % num2;
+                    Console.WriteLine($"Modulas of values is {value}");
+                    break;
+                case 6:
+                    for (int i = num1; i <= num2; i++)
+                    {
+                        value += i;
+                    }
+                    Console.WriteLine($"Fibonacci value is {value}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+                    //throw new Exception("Invalid option");   // we can use this method also
             }
-            Console.WriteLine($"Fibonacci value is {value}");
-            break;
-        default:
+          /*Console.WriteLine($"Answer for selected option is {value}");
+            Console.WriteLine("Press any to continue");
+            Console.ReadLine();
+            Console.Clear(); */
+        }
+        else
+        {
             Console.WriteLine("Invalid option");
-            break;
+        }
     }
-    Console.WriteLine($"Answer for selected option is {value}");
-
-    Console.WriteLine("Press any to continue");
-    Console.ReadLine();
-    Console.Clear();
-    Console.Write("Select from option(-1 to exit)");
-    Console.WriteLine("\n 1.Add \n 2.Sub \n 3.Mul \n 4.Div \n 5.Mod \n 6.Fibonacci ");
-    Console.Write("Enter the option :");
-    option = Convert.ToInt32(Console.ReadLine());
-
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Cannot divide by zero");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    catch (Exception error)
+    {
+        Console.Write(error.Message);
+        //throw;
+    }
+    finally
+    {
+        Console.WriteLine("Press any key to continue");
+        Console.ReadLine();
+        Console.Clear();
+    }
 }
 Console.WriteLine("Thanks for using the calculator");
